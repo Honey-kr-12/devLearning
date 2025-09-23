f81ab9e32ba7bb7e7bca5f4539af0454f9de279d
Honey-kr <shivammahrolia@gmail.com>
2025-09-23 15:39:15 +0530

    add auth

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 import express from "express";
+import logger from "./config/logger.js";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+import authRoutes from './routes/auth.routes.js'
 
 const app = express();
 
 const PORT = process.env.PORT || 8000
 
-app.use('/',(req, res) => res.status(200).send("Hello from Acquisitions"))
+app.use(helmet());
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+
+app.use(morgan('combined',{stream: {write: (message) => logger.info(message.trim())}}))
+app.use('/api/auth', authRoutes)
+app.get('/api',(req, res) => res.status(200).send("new api"))
+app.get('/', (req, res) => {
+    logger.info('Hello from Acquisitions')
+    res.status(200).send("Hello from Acquisitions")
+}
+)
 
 export default app
